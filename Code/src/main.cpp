65d739852595d8ff27a086cc201e8083e3096267
wilsonluk <wilson.luk1@gmail.com>
2020-11-23 04:44:48 -0800

    Inital Interrupt Test

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -7,9 +7,40 @@
 #include <fan.h>
 #include <uvlo.h>
 
+static uint8_t standby = 0;
+
+void suspend() {
+  noInterrupts();
+  Serial.println("Entered Interrupt!");
+  Serial.print("Current State: ");
+  Serial.println(standby);
+  if (standby == 0) {
+    vTaskSuspendAll();
+    for (uint16_t i = 0; i < OCR1A + OCR1B; i++) {
+      if (OCR1A != 0) OCR1A --;
+      if (OCR1B != 0) OCR1B --;
+      delay(1000);
+    }
+    OCR1A = 0;
+    OCR1B = 0;
+    standby = 1;
+    delay(1000);
+    interrupts();
+  } else {
+    standby = 0;
+    delay(1000);
+    interrupts();
+    xTaskResumeAll();
+  }
+  Serial.println("Exited Interrupt!");
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), suspend, RISING);
 
   xTaskCreate(
     uvloTask,
